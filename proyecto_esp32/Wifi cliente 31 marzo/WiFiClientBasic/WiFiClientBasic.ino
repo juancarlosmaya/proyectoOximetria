84a6ffa07f3cf5e87d307f6205e594a5e3d16d20
Ruben Diaz <juancarlosmayag@gmail.com>
2020-04-08 23:30:31 -0500

    primer envio

---
+++ proyecto_esp32/Wifi cliente 31 marzo/WiFiClientBasic/WiFiClientBasic.ino
@@ -0,0 +1,187 @@
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+WiFiMulti WiFiMulti;
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // PONER AQUÌ CLAVE Y CONTRASEÑA
+    WiFiMulti.addAP("ERAZO", "sofia108516#");
+
+    Serial.print("Esperano conexión WIFI");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("");
+    Serial.println("WiFi conectado");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    delay(5000);
+}
+
+
+void loop()
+{
+    bool boleano=false;
+    
+    
+    // SERVIDOR Y PUERTO A DONDE SE VA A CONECTAR EL DISPOSITIVO
+    const uint16_t port = 80;
+    const char server[]= "http://proyecto123.pythonanywhere.com/revisar/examinarusuario/1/";
+    Serial.print("Conectando a: ");
+    Serial.println(server);
+
+
+    // crea un cliente HTTP
+    HTTPClient client;
+
+    // variables auxiliares
+    int i;  // puntero de valor de señal  enviar
+    int vector[] = {85,86,87,88,89,90,91,92,91,90,89,89,90,89,88,87,86,85,84,83}; // señal a enviar
+    int tiempo;
+    
+    //REALIZAR LA CONEXION    
+    if (!client.begin(server)) {
+      // conexión fallida
+      Serial.println("Connection failed.");
+      Serial.println("Waiting 5 seconds before retrying...");
+      delay(5000);
+      return;
+    }
+    else{
+      tiempo=millis();
+      // conexiñon exitosa
+      while (true){                                   
+      for (i=0; i<=19; i++){
+        Serial.println("conexión exitosa");
+        
+        // ---------LEER DATOS
+        int httpCode=client.GET();
+
+        DynamicJsonDocument doc(1024);
+        if (httpCode > 0) {
+          // Get the request response payload
+          String payload = client.getString(); 
+          Serial.println(payload);
+          
+          // obtener los datos de cadena de texto json
+          
+          deserializeJson(doc, payload);
+          JsonObject obj = doc.as<JsonObject>();
+          long frecuencia_estimulacion = obj[String("frecuencia_estimulacion")];
+          Serial.println("la frecuencia de estimulación es");
+          Serial.println(frecuencia_estimulacion);
+          Serial.print("EL TIEMPO TRANSCURRIDO ES");
+          Serial.print(millis()-tiempo);
+          tiempo=millis();
+        }
+
+        
+        // -------- SOBREESCRIBIR datos en api del servidor
+        boleano=not(boleano);
+        client.addHeader("Content-Type", "application/json");
+        //delay(450);
+        String message = "";
+        StaticJsonDocument<300> jsonDoc;
+        //doc[String("nombre")] = "hola";
+        doc[String("frecuencia_estimulacion")] = vector[i];
+        //doc[String("frecuencia_estimulacion")] = 87+7*int(boleano);
+        doc[String("edad")] =60+ 3*int(boleano);
+        
+        serializeJson(doc, message);
+        httpCode = client.PUT(message);
+        //delay(450);
+        
+
+        }
+        }
+        
+    }
+    
+    /*
+
+    //const uint16_t port = 80;
+
+    //const char server[]= "www.google.com";
+    ///const char server[]= "http://proyecto123.pythonanywhere.com/revisar/examinarusuario/1/";
+    //const char * host = "proyecto123.pythonanywhere.com/revisar/examinarusuario/1/";
+
+ 
+    // Use WiFiClient class to create TCP connections
+    
+    //WiFiClient client;
+
+    if (!client.begin(server)) {
+    //if (!client.connect(server, port)) {
+        Serial.println("Connection failed.");
+        Serial.println("Waiting 5 seconds before retrying...");
+        delay(5000);
+        return;
+    }
+  else{                                   
+      Serial.println("conexión exitosa");
+      //http://proyecto123.pythonanywhere.com/revisar/examinar?format=json
+      int httpCode=client.GET();
+
+      if (httpCode > 0) {
+      // Get the request response payload
+      String payload = client.getString(); 
+      Serial.print(payload);
+      // TODO: Parsing
+      }
+
+     
+      //client.print("GET /revisar/examinarusuario/1 HTTP/1.1");
+      //client.println("GET /search?q=arduino HTTP/1.1");
+      //client.println("Host: www.google.com");
+      //client.println("Connection: close");
+      //client.println();
+    }
+
+    
+
+    // This will send a request to the server
+    //uncomment this line to send an arbitrary string to the server
+    //client.print("Send this data to the server");
+    //uncomment this line to send a basic document request to the server
+    //client.print("GET http://proyecto123.pythonanywhere.com/revisar/examinarusuario/1/");
+    
+    //client.print("GET /index.html HTTP/1.1\n\n");
+
+  int maxloops = 0;
+
+  //wait for the server's reply to become available
+//  while (!client.available() && maxloops < 1000)
+//  {
+//    maxloops++;
+//    delay(100); //delay 1 msec
+//  }
+//  while (client.available() > 0)
+//  {
+//    //read back one line from the server
+//    char c = client.read();
+//    Serial.write(c);
+//    //String line = client.readStringUntil('\r');
+//    //Serial.println(line);
+//  }
+
+  
+  //{
+  //  Serial.println("client.available() timed out ");
+  //}
+
+  //  Serial.println("Closing connection.");
+  //  client.stop();
+
+  //  Serial.println("Waiting 5 seconds before restarting...");
+ */
+}
---
+++ python/interfaz gráfica algorimto SpO2.py
@@ -0,0 +1,440 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from time import sleep
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+from drawnow import *
+import tkinter as Tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from scipy.ndimage.interpolation import shift
+
+import csv
+from numpy import genfromtxt
+import tkinter.filedialog
+import tkinter as Tk
+import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, welch, decimate
+from scipy import mean
+
+
+import operator
+
+
+### Diseño de filtro pasa bandas 
+
+def butter_bandpass(lowcut, highcut, fs, order):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+### Diseño de filtro pasa bajos
+
+def butter_lowpass(highcut, fs, order):
+    nyq = 0.5 * fs
+    high = highcut / nyq
+    b, a = butter(order, high, btype='low', )
+    return b, a
+
+
+def butter_lowpass_filter(data, highcut, fs, order):
+    b, a = butter_lowpass(highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def obtenerValorAcDc(senal,fs):
+    #--------------- VALOR DC -------------------
+    ## Filtro a 0.1 Hz 
+    #plt.figure()
+    #plt.plot(senal)
+    
+    senalDC = butter_lowpass_filter(senal, highcut=0.5, fs=fs, order=3)
+    #plt.plot(senalDC)
+    
+    # obtener valorDC
+    valorDC=np.mean(senalDC[100::])
+    senal=senal-valorDC
+    #plt.plot(senal)
+    
+    
+    #--------------- VALOR AC -------------------
+    ## Filtro pasa banda de 0.1 Hz a 20 Hz 
+    #senal=np.hamming(np.size(senal))*senal
+    senalFiltrada = butter_bandpass_filter(senal, highcut=10, lowcut=0.25, fs=fs, order=3)
+    #plt.plot(senalFiltrada)
+    #Submuestreo, ahora la señal queda a 50 Hz
+    senalAC=senalFiltrada[::2]/100
+    tiempoAC=tiempo[::2]
+    fsAC = fs/2
+    
+    senalAC=senalAC[::2]
+    fsAC = fsAC/2
+    tiempoAC=tiempoAC[::2]
+    
+    #plt.plot(senalAC)
+    
+    # obtener y graficar la densidad espectral de potencia
+    senalAC=np.hamming(np.size(senalAC))*senalAC
+    espectroSenalAC=welch(senalAC, fsAC, window='hanning', nperseg=125, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=-1)
+    
+    #plt.figure()
+    
+    #plt.plot(espectroSenalAC[0][0:50],espectroSenalAC[1][0:50])
+    
+    # obtener valorDC
+    indexMaxAC, valueMaxAc = max(enumerate(espectroSenalAC[1]), key=operator.itemgetter(1))
+    FrecuenciaMaxima=espectroSenalAC[0][indexMaxAC]
+    valorAC=valueMaxAc
+
+    return valorDC,valorAC,FrecuenciaMaxima
+
+
+
+# Adquirir y procesar señales
+def analizarSerial():
+    print("hola")
+
+    
+    
+    # Apertura comunicación serial
+    ser = serial.Serial('/dev/cu.usbserial-0001', 115200) # Establish the connection on a specific port
+    #ser.open()
+    
+    while (ser.inWaiting()==0): #Wait here until there is data
+        pass #do nothing
+    
+    #ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+    
+    # Retardo para que lleguen el mensaje de inicialización de ESP32
+    sleep(3)
+    
+    while (ser.inWaiting()==0): #Wait here until there is data
+        pass #do nothing
+   
+    # Se lee el mensaje de inicio de ESP32, varias líneas
+    arduinoString=ser.readline()
+    rduinoString=ser.readline()
+    rduinoString=ser.readline()
+    rduinoString=ser.readline()
+    rduinoString=ser.readline()
+    rduinoString=ser.readline()
+    rduinoString=ser.readline()
+
+    # Configuracíon de ejes de gráficas
+    ejex=np.arange(0,5,0.01)
+    ejeyRojo=np.zeros(500)
+    ejeyInfrarojo=np.zeros(500)
+    
+    # Se graficarán 1000 muestras de cada señal, rojo e infrarojo
+    for x in range(0, 1000, 1):
+        
+       
+        while (ser.inWaiting()==0): #Wait here until there is data
+            pass #do nothing
+        
+
+        
+        
+        ## LEER DATOS DEL PUERTO SERIAL, rojo e infrarojo se encuntran separados por una ","
+        arduinoString=ser.readline() # Read the newest output from the Arduino
+        arduinoString=arduinoString.decode('utf-8')
+        print(arduinoString)
+        dataArray = arduinoString.split(',') 
+        rojo = float( dataArray[0])            
+        infraRojo = float( dataArray[1])
+        
+        
+        ejeyRojo=shift(ejeyRojo, -1, cval=0)
+        ejeyRojo[499]=rojo
+        
+        ejeyInfrarojo=shift(ejeyInfrarojo, -1, cval=0)
+        ejeyInfrarojo[499]=infraRojo
+        
+        
+        # Graficar Rojo
+        figuraRojo.cla()
+        figuraRojo.plot(ejex,ejeyRojo,color="red")
+        figuraRojo.set_title('Señal Led Rojo',fontsize=20)
+        figuraRojo.set_xlabel('Tiempo /s',fontsize=16)
+        figuraRojo.set_ylabel('Voltaje /v',fontsize=16)
+        canvas1.draw()
+        
+        # Graficar Infrarojo
+        figuraInfrarojo.cla()
+        figuraInfrarojo.plot(ejex,ejeyInfrarojo)#,'ro',markersize=1, color="blue")
+        figuraInfrarojo.set_title('Señal Led Infrarojo',fontsize=20)
+        figuraInfrarojo.set_xlabel('Tiempo /s',fontsize=16)
+        figuraInfrarojo.set_ylabel('Voltaje /v',fontsize=16)
+        canvas2.draw()
+        
+        
+        root.update()
+        sleep(0.01)  #equivale a 250Hz
+    
+def analizarCsv():
+    
+    plt.cla()
+
+    fs=100 # Frecuencia de muestreo
+
+    directorio="/Users/rubendiaz/Desktop/proyecto oximetría/python/datos800.csv"
+
+    datosLeidos=genfromtxt(directorio, delimiter=';')
+
+    
+
+    m=0  
+    print("##################################################")
+    print()
+    print("Tomando las muestras entre " + str(m)+ " y "+  str(m+5000)) 
+    print()
+    tiempo=datosLeidos[:,0][m:m+5000]
+    senalIR=datosLeidos[:,2][m:m+5000]
+    #plt.plot(senalIR)
+    senalRojo=datosLeidos[:,3][m:m+5000]
+    #plt.plot(senalRojo)
+    #plt.figure()
+    
+    # Configuracíon de ejes de gráficas
+    ejex=np.arange(0,40,0.04)
+    ejeyRojo=np.zeros(500)
+    ejeyInfrarojo=np.zeros(500)
+
+    ejeyRojo2=np.zeros(1000)
+    ejeyInfrarojo2=np.zeros(1000)    
+    # Se graficarán 1000 muestras de cada señal, rojo e infrarojo
+    for x in range(0, 4000, 1):
+ 
+        rojo = senalRojo[x]          
+        infraRojo = senalIR[x]
+        
+        
+        ejeyRojo=shift(ejeyRojo, -1, cval=0)
+        ejeyRojo[499]=rojo
+        
+        ejeyInfrarojo=shift(ejeyInfrarojo, -1, cval=0)
+        ejeyInfrarojo[499]=infraRojo
+        
+        
+        
+        senal=ejeyRojo
+        ###################### ROJO ###########################################
+        #--------------- VALOR DC -------------------
+        ## Filtro a 0.1 Hz 
+        #plt.figure()
+        #plt.plot(senal)
+    
+        senalDC = butter_lowpass_filter(senal, highcut=0.5, fs=fs, order=3)
+        #plt.plot(senalDC)
+    
+        # obtener valorDC
+        valorDC=np.mean(senalDC[100::])
+        senal=senal-valorDC
+        #plt.plot(senal)
+    
+    
+        #--------------- VALOR AC -------------------
+        ## Filtro pasa banda de 0.1 Hz a 20 Hz 
+        #senal=np.hamming(np.size(senal))*senal
+        senalFiltrada = butter_bandpass_filter(senal, highcut=10, lowcut=0.25, fs=fs, order=3)
+        #plt.plot(senalFiltrada)
+        #Submuestreo, ahora la señal queda a 50 Hz
+        senalAC=senalFiltrada[::2]/100
+        tiempoAC=tiempo[::2]
+        fsAC = fs/2
+        
+        senalAC=senalAC[::2]
+        fsAC = fsAC/2
+        tiempoAC=tiempoAC[::2]
+        
+        #plt.plot(senalAC)
+        
+        # obtener y graficar la densidad espectral de potencia
+        espectroSenalAC=welch(senalAC, fsAC, window='hanning', nperseg=125, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=-1)
+        
+        #plt.figure()
+        
+        #plt.plot(espectroSenalAC[0][0:50],espectroSenalAC[1][0:50])
+        
+        # obtener valorDC
+        indexMaxAC, valueMaxAc = max(enumerate(espectroSenalAC[1]), key=operator.itemgetter(1))
+        FrecuenciaMaxima=espectroSenalAC[0][indexMaxAC]
+        valorAC=valueMaxAc
+        
+        if abs(senalAC[124])<20:
+            ejeyRojo2=shift(ejeyRojo2, -1, cval=0)
+            ejeyRojo2[-1]=senalAC[124]
+        
+
+        
+        
+        # Graficar Rojo
+        figuraRojo.cla()
+        figuraRojo.plot(ejex,ejeyRojo2,color="red")
+        figuraRojo.set_title('Señal Led Rojo',fontsize=20)
+        #figuraRojo.set_xlabel('Tiempo /s',fontsize=16)
+        #figuraRojo.set_ylabel('Voltaje /v',fontsize=16)
+        canvas1.draw()
+        #######################################################################
+        
+        
+        senal=ejeyInfrarojo
+        ###################### Infrarojo ###########################################
+        #--------------- VALOR DC -------------------
+        ## Filtro a 0.1 Hz 
+        #plt.figure()
+        #plt.plot(senal)
+    
+        senalDC = butter_lowpass_filter(senal, highcut=0.5, fs=fs, order=3)
+        #plt.plot(senalDC)
+    
+        # obtener valorDC
+        valorDC=np.mean(senalDC[100::])
+        senal=senal-valorDC
+        #plt.plot(senal)
+    
+    
+        #--------------- VALOR AC -------------------
+        ## Filtro pasa banda de 0.1 Hz a 20 Hz 
+        #senal=np.hamming(np.size(senal))*senal
+        senalFiltrada = butter_bandpass_filter(senal, highcut=10, lowcut=0.25, fs=fs, order=3)
+        #plt.plot(senalFiltrada)
+        #Submuestreo, ahora la señal queda a 50 Hz
+        senalAC=senalFiltrada[::2]/100
+        tiempoAC=tiempo[::2]
+        fsAC = fs/2
+        
+        senalAC=senalAC[::2]
+        fsAC = fsAC/2
+        tiempoAC=tiempoAC[::2]
+        
+        #plt.plot(senalAC)
+        
+        # obtener y graficar la densidad espectral de potencia
+        #senalAC=np.hamming(np.size(senalAC))*senalAC
+        espectroSenalAC=welch(senalAC, fsAC, window='hanning', nperseg=125, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=-1)
+        
+        #plt.figure()
+        
+        #plt.plot(espectroSenalAC[0][0:50],espectroSenalAC[1][0:50])
+        
+        # obtener valorDC
+        indexMaxAC, valueMaxAc = max(enumerate(espectroSenalAC[1]), key=operator.itemgetter(1))
+        FrecuenciaMaxima=espectroSenalAC[0][indexMaxAC]
+        valorAC=valueMaxAc
+        
+        if abs(senalAC[124])<20:
+            ejeyInfrarojo2=shift(ejeyInfrarojo2, -1, cval=0)
+            ejeyInfrarojo2[-1]=senalAC[124]
+        
+        # Graficar Infrarojo
+        figuraInfrarojo.cla()
+        figuraInfrarojo.plot(ejex,ejeyInfrarojo2)#,'ro',markersize=1, color="blue")
+        figuraInfrarojo.set_title('Señal Led Infrarojo',fontsize=20)
+        #figuraInfrarojo.set_xlabel('Tiempo /s',fontsize=16)
+        #figuraInfrarojo.set_ylabel('Voltaje /v',fontsize=16)
+        canvas2.draw()
+        #######################################################################
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+
+        
+        
+        root.update()
+        #sleep(0.01)  #equivale a 250Hz
+    
+    
+    
+    # [valorDCIR,valorACIR,frecuenciaCardiacaHzIR]=obtenerValorAcDc(senalIR,fs)
+    
+    # print("el valor DC IR es:"+str(valorDCIR))
+    # print("el valor AC IR es: "+str(valorACIR))
+    # print("la frecuencia cardíaca IR es:"+str(frecuenciaCardiacaHzIR*60))
+    # print()
+    
+    # [valorDCRojo,valorACRojo,frecuenciaCardiacaHzRojo]=obtenerValorAcDc(senalRojo,fs)
+    
+    # print("el valor DC Rojo es:"+str(valorDCRojo))
+    # print("el valor AC Rojo es: "+str(valorACRojo))
+    # print("la frecuencia cardíaca Rojo es:"+str(frecuenciaCardiacaHzRojo*60))
+    # print()
+    
+    # R=(valorACRojo/valorDCRojo)/(valorACIR/valorDCIR)
+    # print("el valor R es:"+str(R))
+    # print()
+    # R=R/6
+    # spo2=110-25*R
+    # print("el valor de la SpO2 es:"+str(spo2))
+    # print()
+    
+
+
+#----------------- CONFIGURACIÓN INTERFAZ GRÁFICA
+
+root = Tk.Tk()
+root.title("Clasificador Señal Ecg Normal y Fibrilacion Auricula AF")
+Tk.Label(root,text="Seleccione opcion en el Menú Archivo").pack(side=Tk.TOP)
+
+# Figura Rojo
+figura1 = Figure(figsize=(5, 3), dpi=60)
+figuraRojo = figura1.add_subplot(211)
+figuraRojo.set_title('Señal Led Rojo',fontsize=20)
+figuraRojo.set_xlabel('Tiempo /s',fontsize=16)
+figuraRojo.set_ylabel('Voltaje /v',fontsize=16)
+canvas1 = FigureCanvasTkAgg(figura1, master=root)
+canvas1.draw()
+canvas1.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1,padx=1,pady=1)
+canvas1._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1,padx=1,pady=1)
+
+# Figura Infrarojo
+figura2 = Figure(figsize=(5, 3), dpi=60)
+figuraInfrarojo = figura1.add_subplot(211)
+figuraInfrarojo.set_title('Señal Led Infrarojo',fontsize=20)
+figuraInfrarojo.set_xlabel('Tiempo /s',fontsize=16)
+figuraInfrarojo.set_ylabel('Voltaje /v',fontsize=16)
+canvas2 = FigureCanvasTkAgg(figura1, master=root)
+canvas2.draw()
+canvas2.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1,padx=1,pady=1)
+canvas2._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1,padx=1,pady=1)
+
+# Menú
+bar=Tk.Menu(root)
+fileMenu=Tk.Menu(bar,tearoff=0)
+fileMenu.add_command(label="Analizar señal via puerto serial",command=analizarSerial)
+fileMenu.add_command(label="Analizar archivo csv",command=analizarCsv)
+fileMenu.add_command(label="Salir")
+bar.add_cascade(label="Archivo",menu=fileMenu)
+root.config(menu=bar)
+
+# El programa queda esperando a un evento de la GUI
+Tk.mainloop()
+        
+        
+        
